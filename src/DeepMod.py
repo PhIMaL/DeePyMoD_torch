5fecee3276deed473d8e62b80b2365c371c59660
Gert-Jan <gert-jan.both@cri-paris.com>
2019-07-01 16:27:26 +0200

    Updated neural net function

--- src/DeepMod.py
+++ src/DeepMod.py
@@ -7,53 +7,53 @@ from neural_net import *
 from sparsity import scaling, threshold
 
 
-def DeepMod(data, target, network_config, library_type, library_config, optim_config, init_coef):   
-    
+def DeepMod(data, target, network_config, library_type, library_config, optim_config, coeff_vec):
+
     # Initiate neural network, weight_vector and optimizer:
-    network = LinNetwork(network_config)    
-    
+    network = network_init(network_config)
+
     # Training of the network
-    y_t,theta, weight_vector = Training(data, target, optim_config, library_type, library_config, network, network_config, init_coef)
-    
+    y_t, theta, weight_vector = train(data, target, network, coeff_vec, library_config, optim_config)
+
     # Scaling
     scaled_weight_vector = scaling(y_t,theta, weight_vector)
     print(scaled_weight_vector)
 
     # Thresholding
-    sparse_weight_vector, sparsity_mask =  threshold(scaled_weight_vector,weight_vector)    
+    sparse_weight_vector, sparsity_mask =  threshold(scaled_weight_vector,weight_vector)
     print(sparsity_mask)
-    
+
     # Final Training without L1 and with the sparsity pattern
     sparse_weight_vector, prediction = Final_Training(data, target, optim_config, library_config, network, network_config, sparse_weight_vector, sparsity_mask)
-    
+
     return sparse_weight_vector, sparsity_mask, prediction, network
 
 
-def DeepMod_mse(data, target, network_config, library_type, library_config, optim_config):   
-    
+def DeepMod_mse(data, target, network_config, library_type, library_config, optim_config):
+
     # Initiate neural network, weight_vector and optimizer:
-    network = LinNetwork(network_config)    
-    
+    network = LinNetwork(network_config)
+
     # Training of the network
     prediction, network, y_t, theta = Training_MSE(data, target, optim_config, library_type, library_config, network, network_config)
 
-    
+
     return prediction, network, y_t, theta
 
 
-def DeepMod_pretrained_nomse(data, target, network_config, library_type, library_config, optim_config, network,init_coeff):   
-        
+def DeepMod_pretrained_nomse(data, target, network_config, library_type, library_config, optim_config, network,init_coeff):
+
     # Training of the network
     y_t,theta, weight_vector = Training_PI(data, target, optim_config, library_type, library_config, network, network_config,init_coeff)
 
-    
+
     return  y_t,theta, weight_vector
 
 
-def DeepMod_single(data, target, network_config, library_type, library_config, optim_config, network,init_coeff):   
-        
+def DeepMod_single(data, target, network_config, library_type, library_config, optim_config, network,init_coeff):
+
     # Training of the network
     y_t,theta, weight_vector = Training(data, target, optim_config, library_type, library_config, network, network_config,init_coeff)
 
-    
+
     return  y_t,theta, weight_vector
--- src/neural_net.py
+++ src/neural_net.py
@@ -4,173 +4,177 @@ import torch
 
 from library_function import *
 
-def LinNetwork(network_config): # network config
-    
-    input_dim=network_config['input_dim'] 
-    hidden_dim=network_config['hidden_dim'] 
-    layers=network_config['layers']
-    output_dim=network_config['output_dim']
-    
-    modules = []
-    modules.append(nn.Linear(input_dim, hidden_dim))
-    modules.append(nn.Tanh())
-    for i in np.arange(0,layers):
-        modules.append(nn.Linear(hidden_dim, hidden_dim))
-        modules.append(nn.Tanh())
-    modules.append(nn.Linear(hidden_dim, output_dim))
-    sequential = nn.Sequential(*modules)
-    
-    return sequential
 
-def Training(data, target, optim_config, library_type, library_config, network, network_config, init_coef):
-    
-    max_it = optim_config['max_iteration']
+def network_init(network_config):
+    '''
+    Builds a fully-connected NN according to specified parameters.
+    '''
+    # unpacking configuration
+    input_dim = network_config['input_dim']
+    hidden_dim = network_config['hidden_dim']
+    layers = network_config['layers']
+    output_dim = network_config['output_dim']
+
+    # Building network
+    network = [nn.Linear(input_dim, hidden_dim), nn.Tanh()]  # Input layer
+
+    for hidden_layer in np.arange(layers):  # Hidden layers
+        network.append(nn.Linear(hidden_dim, hidden_dim))
+        network.append(nn.Tanh())
+
+    network.append(nn.Linear(hidden_dim, output_dim))  # Output layer
+    torch_network = nn.Sequential(*network)
+
+    return torch_network
+
+
+def train(data, target, network, coeff_vector, library_config, optim_config):
+    '''
+    Trains deepmod NN.
+    '''
+
+    max_it = optim_config['max_iterations']
     l1 = optim_config['lambda']
-    
-    weight_vector = init_coef
-    # Initialize the weight vector and optimizer 
-    
-    optimizer = torch.optim.Adam([{'params':network.parameters()}, {'params': weight_vector}])
-    
-    for iteration in np.arange(max_it):
-        
-        # Calculate the predicted y-value, construct the library function
-        prediction = network(data)    
-        if optim_config['type'] == PI or Single:
-            y_t, theta = library_type(data, prediction,library_config)
-            f = y_t - theta @ weight_vector
+    library_function = library_config['type']
 
-        # Losses: MSE, PI and L1  
+    optimizer = torch.optim.Adam([{'params': network.parameters()}, {'params': coeff_vector}])
+
+    for iteration in np.arange(max_it):
+
+        # Calculate the predicted y-value, construct the library function
+        prediction = network(data)
+        y_t, theta = library_function(data, prediction,library_config)
+        f = y_t - theta @ coeff_vector
+
+        # Losses: MSE, PI and L1
 
         loss_MSE = torch.nn.MSELoss()(prediction, target)
         loss_PI = torch.nn.MSELoss()(f, torch.zeros_like(y_t))
-        loss_L1 = l1*nn.L1Loss()(weight_vector,torch.zeros_like(weight_vector))
-        
+        loss_L1 = l1*nn.L1Loss()(coeff_vector,torch.zeros_like(coeff_vector))
+
         # Combine all the losses
-        loss = loss_MSE + loss_PI + loss_L1 
-        
+        loss = loss_MSE + loss_PI + loss_L1
+
         # Optimizwe step
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        # Print the losses during training 
+
+        # Print the losses during training
         if iteration == 0:
             print('Epoch | Total loss | MSE | PI | L1 ')
         if iteration % 1000 == 0:
             print(iteration, "%.1E" % loss.detach().numpy(), "%.1E" % loss_MSE.detach().numpy(), "%.1E" % loss_PI.detach().numpy(), "%.1E" % loss_L1.detach().numpy())
-            print(np.around(weight_vector.detach().numpy(),decimals=2))
-            
-    return y_t, theta, weight_vector
+            print(np.around(coeff_vector.detach().numpy(),decimals=2))
+
+    return y_t, theta, coeff_vector
 
 
 def Final_Training(data, target, optim_config, library_type, library_config, network, network_config, sparse_weight_vector, sparsity_mask):
-    
+
     max_it = 5000
-    
-    # Re-initialize the optimizer 
+
+    # Re-initialize the optimizer
     optimizer = torch.optim.Adam([{'params':network.parameters()}, {'params': sparse_weight_vector}])
-    
+
     for iteration in np.arange(max_it):
-        
+
         # Calculate the predicted y-value, construct the sparse library function
-        prediction = network(data)    
+        prediction = network(data)
         y_t, theta = library_poly_multi(data, prediction,library_config)
-        
+
         # Use a dummy vector to flatten the coefficient vector and apply the mask (This could be done more elegantly)
         dummy = torch.zeros((library_config['total_terms']*network_config['output_dim'],1))
         dummy[sparsity_mask] = sparse_weight_vector
         dummy = dummy.reshape(library_config['total_terms'], network_config['output_dim'])
         f = y_t - theta @ dummy
-        
-        # Losses: MSE, PI and L1  
+
+        # Losses: MSE, PI and L1
         loss_MSE = torch.nn.MSELoss()(prediction, target)
         loss_PI = torch.nn.MSELoss()(f, torch.zeros_like(y_t))
-        
+
         # Combine all the losses
         loss = loss_MSE + loss_PI
-        
+
         # Optimizwe step
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        # Print the losses during training 
+
+        # Print the losses during training
         if iteration == 0:
             print('Epoch | Total loss | MSE | PI ')
         if iteration % 1000 == 0:
             print(iteration, "%.1Ef" % loss.detach().numpy(), "%.1E" % loss_MSE.detach().numpy(), "%.1E" % loss_PI.detach().numpy())
             print(np.around(sparse_weight_vector.detach().numpy(),decimals=2).reshape(1,-1))
-            
+
     return sparse_weight_vector, prediction
 
 
 def Training_MSE(data, target, optim_config, library_type, library_config, network, network_config):
-    
+
     max_it = optim_config['max_iteration']
     l1 = optim_config['lambda']
-    
-    # Initialize the weight vector and optimizer 
+
+    # Initialize the weight vector and optimizer
 
     optimizer = torch.optim.Adam([{'params':network.parameters()}])
-    
+
     for iteration in np.arange(max_it):
-        
-        prediction = network(data)    
+
+        prediction = network(data)
         # Combine all the losses
         loss = torch.nn.MSELoss()(prediction, target)
-        y_t, theta = library_1D(data, prediction,library_config)   
- 
+        y_t, theta = library_1D(data, prediction,library_config)
+
         # Optimizwe step
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        # Print the losses during training 
+
+        # Print the losses during training
         if iteration == 0:
             print('Epoch | Total loss | MSE ')
         if iteration % 1000 == 0:
             print(iteration, "%.1E" % loss.detach().numpy(), "%.1E" % loss.detach().numpy())
-            
+
     return prediction, network, y_t, theta
 
 
 def Training_PI(data, target, optim_config, library_type, library_config, network, network_config,init_coeff):
-    
+
     max_it = optim_config['max_iteration']
     l1 = optim_config['lambda']
-    
-    # Initialize the weight vector and optimizer 
+
+    # Initialize the weight vector and optimizer
     weight_vector = init_coeff
     optimizer = torch.optim.Adam([{'params': weight_vector}])
-    
+
     for iteration in np.arange(max_it):
-        
+
         # Calculate the predicted y-value, construct the library function
-        prediction = network(data)    
+        prediction = network(data)
         y_t, theta = library_type(data, prediction,library_config)
         f = y_t - theta @ weight_vector
 
-        # Losses: MSE, PI and L1  
+        # Losses: MSE, PI and L1
         loss_MSE = torch.nn.MSELoss()(prediction, target)
         loss_PI = torch.nn.MSELoss()(f, torch.zeros_like(y_t))
         loss_L1 = l1*nn.L1Loss()(weight_vector,torch.zeros_like(weight_vector))
-        
+
         # Combine all the losses
-        loss = loss_MSE + loss_PI + loss_L1 
-        
+        loss = loss_MSE + loss_PI + loss_L1
+
         # Optimizwe step
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        # Print the losses during training 
+
+        # Print the losses during training
         if iteration == 0:
             print('Epoch | Total loss | MSE | PI | L1 ')
         if iteration % 1000 == 0:
             print(iteration, "%.1E" % loss.detach().numpy(), "%.1E" % loss_MSE.detach().numpy(), "%.1E" % loss_PI.detach().numpy(), "%.1E" % loss_L1.detach().numpy())
             print(np.around(weight_vector.detach().numpy(),decimals=2))
-            
+
     return y_t, theta, weight_vector
-
-
