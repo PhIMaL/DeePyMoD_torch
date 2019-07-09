4a9795479557b0792e9b3566a3bc7c981087b88d
Gert-Jan <gertjanboth@gmail.com>
2019-07-09 17:39:45 +0200

    L1 turned off for last cycle.

--- src/deepymod_torch/DeepMod.py
+++ src/deepymod_torch/DeepMod.py
@@ -3,11 +3,12 @@ from deepymod_torch.sparsity import scaling, threshold
 
 
 def DeepMoD(data, target, network_config, library_config, optim_config):
+    optim_config_internal = optim_config.copy()
     # Initiating
     network, coeff_vector_list, sparsity_mask_list = deepmod_init(network_config, library_config)
     
     # Training of the network
-    time_deriv_list, theta, coeff_vector_list = train(data, target, network, coeff_vector_list, sparsity_mask_list, library_config, optim_config)
+    time_deriv_list, theta, coeff_vector_list = train(data, target, network, coeff_vector_list, sparsity_mask_list, library_config, optim_config_internal)
     
     # Thresholding
     scaled_coeff_vector_list = [scaling(coeff_vector, theta, time_deriv) for coeff_vector, time_deriv in zip(coeff_vector_list, time_deriv_list)]
@@ -15,8 +16,10 @@ def DeepMoD(data, target, network_config, library_config, optim_config):
     
     print(coeff_vector_list, sparse_coeff_vector_list, sparsity_mask_list)
     print('Now running final cycle.')
+
     # Final Training without L1 and with the sparsity pattern
-    time_deriv_list, theta, coeff_vector_list = train(data, target, network, sparse_coeff_vector_list, sparsity_mask_list, library_config, optim_config)
+    optim_config_internal['lambda'] = 0
+    time_deriv_list, theta, coeff_vector_list = train(data, target, network, sparse_coeff_vector_list, sparsity_mask_list, library_config, optim_config_internal)
 
     return coeff_vector_list, sparsity_mask_list, network
     
