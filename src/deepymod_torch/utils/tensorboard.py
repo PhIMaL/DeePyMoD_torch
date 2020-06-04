709a5003a24e3f128fb30d890bc1024b1c9b325d
Gert-Jan <gertjanboth@gmail.com>
2020-06-04 14:03:36 +0200

    Tensorboard classes now use f strings.

--- src/deepymod_torch/utils/tensorboard.py
+++ src/deepymod_torch/utils/tensorboard.py
@@ -13,13 +13,13 @@ class Tensorboard():
         # Stuff for custom board.
         self.writer.add_scalar('Total loss', loss, iteration)
         for idx in range(len(loss_mse)):
-            self.writer.add_scalar('MSE '+str(idx), loss_mse[idx], iteration)
-            self.writer.add_scalar('Regression '+str(idx), loss_reg[idx], iteration)
-            self.writer.add_scalar('L1 '+str(idx), loss_l1[idx], iteration)
+            self.writer.add_scalar(f'MSE {idx}', loss_mse[idx], iteration)
+            self.writer.add_scalar(f'Regression {idx}', loss_reg[idx], iteration)
+            self.writer.add_scalar(f'L1 {idx}', loss_l1[idx], iteration)
             for element_idx, element in enumerate(torch.unbind(coeff_vector_list[idx])):  # Tensorboard doesnt have vectors, so we unbind and plot them in together in custom board
-                self.writer.add_scalar('coeff ' + str(idx) + ' ' + str(element_idx), element, iteration)
+                self.writer.add_scalar(f'coeff {idx} {element_idx}', element, iteration)
             for element_idx, element in enumerate(torch.unbind(coeff_vector_scaled_list[idx])):
-                self.writer.add_scalar('scaled_coeff ' + str(idx) + ' ' + str(element_idx), element, iteration)
+                self.writer.add_scalar(f'scaled_coeff {idx} {element_idx}', element, iteration)
 
         # Writing remaining kwargs
         for key, value in kwargs.items():
@@ -38,15 +38,15 @@ def custom_board(number_of_terms):
     '''Custom scalar board for tensorboard.'''
     number_of_eqs = len(number_of_terms)
     # Initial setup, including all the costs and losses
-    custom_board = {'Costs': {'MSE': ['Multiline', ['MSE_' + str(idx) for idx in np.arange(number_of_eqs)]],
-                              'Regression': ['Multiline', ['Regression_' + str(idx) for idx in np.arange(number_of_eqs)]],
-                              'L1': ['Multiline', ['L1_' + str(idx) for idx in np.arange(number_of_eqs)]]},
+    custom_board = {'Costs': {'MSE': ['Multiline', [f'MSE_{idx}' for idx in np.arange(number_of_eqs)]],
+                              'Regression': ['Multiline', [f'Regression_{idx}' for idx in np.arange(number_of_eqs)]],
+                              'L1': ['Multiline', [f'L1_{idx}' for idx in np.arange(number_of_eqs)]]},
                     'Coefficients': {},
                     'Scaled coefficients': {}}
 
     # Add plot of normal and scaled coefficients for each equation, containing every component in single plot.
     for idx in np.arange(number_of_eqs):
-        custom_board['Coefficients']['Vector_' + str(idx)] = ['Multiline', ['coeff_' + str(idx) + '_' + str(element_idx) for element_idx in np.arange(number_of_terms[idx])]]
-        custom_board['Scaled coefficients']['Vector_' + str(idx)] = ['Multiline', ['scaled_coeff_' + str(idx) + '_' + str(element_idx) for element_idx in np.arange(number_of_terms[idx])]]
+        custom_board['Coefficients'][f'Vector_{idx}'] = ['Multiline', [f'coeff_{idx}_{element_idx}' for element_idx in np.arange(number_of_terms[idx])]]
+        custom_board['Scaled coefficients'][f'Vector_{idx}'] = ['Multiline', [f'scaled_coeff_{idx}_{element_idx}' for element_idx in np.arange(number_of_terms[idx])]]
 
     return custom_board
