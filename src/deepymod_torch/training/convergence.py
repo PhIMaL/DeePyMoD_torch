d983458b053e9db129e8692871f5dc33018f3e19
Gert-Jan <gertjanboth@gmail.com>
2020-06-05 11:55:46 +0200

    Added convergence criterium

---
+++ src/deepymod_torch/training/convergence.py
@@ -0,0 +1,23 @@
+import torch
+
+
+class Convergence:
+    def __init__(self, patience=100, delta=0.05):
+        self.patience = patience
+        self.delta = delta
+        self.counter = 0
+        self.start_l1 = None
+        self.converged = False
+
+    def __call__(self, epoch, l1_norm):
+        if self.start_l1 is None:
+            self.start_l1 = l1_norm
+        elif torch.abs(self.start_l1 - l1_norm).item() < self.delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.converged = True
+        else:
+            self.start_l1 = l1_norm
+            self.counter = 0
+        return self.converged
+        
--- src/deepymod_torch/training/training.py
+++ src/deepymod_torch/training/training.py
@@ -4,21 +4,20 @@ from math import pi
 
 from deepymod_torch.utils.tensorboard import Tensorboard
 from deepymod_torch.utils.output import progress
+from deepymod_torch.training.convergence import Convergence
 
-
-def train(model, data, target, optimizer, sparsity_scheduler, log_dir=None, max_iterations=10000):
+def train(model, data, target, optimizer, sparsity_scheduler, log_dir=None, max_iterations=10000, **convergence_kwargs):
     start_time = time.time()
     number_of_terms = [coeff_vec.shape[0] for coeff_vec in model(data)[3]]
     board = Tensorboard(number_of_terms, log_dir)  # initializing custom tb board
 
     # Training
+    convergence = Convergence(**convergence_kwargs)
     print('| Iteration | Progress | Time remaining |     Loss |      MSE |      Reg |    L1 norm |')
     for iteration in torch.arange(0, max_iterations + 1):
         # ================== Training Model ============================
-
         prediction, time_derivs, sparse_thetas, thetas, constraint_coeffs = model(data)
-        l1_norm = torch.stack([torch.sum(torch.abs(coeff_vector)) for coeff_vector in constraint_coeffs])
-
+        
         MSE = torch.mean((prediction - target)**2, dim=0)  # loss per output
         Reg = torch.stack([torch.mean((dt - theta @ coeff_vector)**2)
                            for dt, theta, coeff_vector in zip(time_derivs, sparse_thetas, constraint_coeffs)])
@@ -31,6 +30,7 @@ def train(model, data, target, optimizer, sparsity_scheduler, log_dir=None, max_
 
         # ====================== Logging =======================
         # Write progress to command line and tensorboard
+        l1_norm = torch.stack([torch.sum(torch.abs(coeff_vector)) for coeff_vector in constraint_coeffs])
         if iteration % 25 == 0:
             progress(iteration, start_time, max_iterations, loss.item(),
                      torch.sum(MSE).item(), torch.sum(Reg).item(), torch.sum(l1_norm).item())
@@ -53,4 +53,11 @@ def train(model, data, target, optimizer, sparsity_scheduler, log_dir=None, max_
                 model.constraint.sparsity_masks = model.sparse_estimator(thetas, time_derivs)
                 sparsity_scheduler.reset()
                 print(model.constraint.sparsity_masks)
+
+        # Checking convergence
+        converged = convergence(iteration, torch.sum(l1_norm))
+        if converged:
+            print('Algorithm converged. Stopping training.')
+            break
+
     board.close()
