066c721edcc64e2fe68cc2122ca8435ba6be4edb
Gert-Jan <gertjanboth@gmail.cmo>
2020-07-23 11:54:00 +0200

    Made all tensorboard tags lowercase

--- src/deepymod_torch/utils/tensorboard.py
+++ src/deepymod_torch/utils/tensorboard.py
@@ -1,6 +1,5 @@
 from torch.utils.tensorboard import SummaryWriter
 
-
 class Tensorboard():
     '''Tensorboard class for logging during deepmod training. '''
     def __init__(self, log_dir):
@@ -10,22 +9,22 @@ class Tensorboard():
     def write(self, iteration, loss, loss_mse, loss_reg, loss_l1,
               constraint_coeff_vectors, unscaled_constraint_coeff_vectors, estimator_coeff_vectors, **kwargs):
         # Costs and coeff vectors
-        self.writer.add_scalar('Loss/Loss', loss, iteration)
-        self.writer.add_scalars('Loss/MSE', {f'Output_{idx}': val for idx, val in enumerate(loss_mse)}, iteration)
-        self.writer.add_scalars('Loss/Reg', {f'Output_{idx}': val for idx, val in enumerate(loss_reg)}, iteration)
-        self.writer.add_scalars('Loss/L1', {f'Output_{idx}': val for idx, val in enumerate(loss_l1)}, iteration)
+        self.writer.add_scalar('loss/loss', loss, iteration)
+        self.writer.add_scalars('loss/mse', {f'output_{idx}': val for idx, val in enumerate(loss_mse)}, iteration)
+        self.writer.add_scalars('loss/reg', {f'output_{idx}': val for idx, val in enumerate(loss_reg)}, iteration)
+        self.writer.add_scalars('loss/l1', {f'output_{idx}': val for idx, val in enumerate(loss_l1)}, iteration)
 
         for output_idx, (coeffs, unscaled_coeffs, estimator_coeffs) in enumerate(zip(constraint_coeff_vectors, unscaled_constraint_coeff_vectors, estimator_coeff_vectors)):
-            self.writer.add_scalars(f'Coeffs/Output_{output_idx}', {f'Coeff_{idx}': val for idx, val in enumerate(coeffs.squeeze())}, iteration)
-            self.writer.add_scalars(f'Unscaled coeffs/Output_{output_idx}', {f'Coeff_{idx}': val for idx, val in enumerate(unscaled_coeffs.squeeze())}, iteration)
-            self.writer.add_scalars(f'Estimator coeffs/Output_{output_idx}', {f'Coeff_{idx}': val for idx, val in enumerate(estimator_coeffs.squeeze())}, iteration)
+            self.writer.add_scalars(f'coeffs/output_{output_idx}', {f'coeff_{idx}': val for idx, val in enumerate(coeffs.squeeze())}, iteration)
+            self.writer.add_scalars(f'unscaled_coeffs/output_{output_idx}', {f'coeff_{idx}': val for idx, val in enumerate(unscaled_coeffs.squeeze())}, iteration)
+            self.writer.add_scalars(f'estimator_coeffs/output_{output_idx}', {f'coeff_{idx}': val for idx, val in enumerate(estimator_coeffs.squeeze())}, iteration)
 
         # Writing remaining kwargs
         for key, value in kwargs.items():
             if value.numel() == 1:
-                self.writer.add_scalar(f'Remaining/{key}', value, iteration)
+                self.writer.add_scalar(f'remaining/{key}', value, iteration)
             else:
-                self.writer.add_scalars(f'Remaining/{key}', {f'val_{idx}': val.squeeze() for idx, val in enumerate(value.squeeze())}, iteration)
+                self.writer.add_scalars(f'remaining/{key}', {f'val_{idx}': val.squeeze() for idx, val in enumerate(value.squeeze())}, iteration)
 
     def close(self):
         self.writer.flush()  # flush remaining stuff to disk
